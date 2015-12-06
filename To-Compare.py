99772b5462262cd62cbdaeaf50f792661f362d88
Kayla Nguyen <nguye27k@mtholyoke.edu>
2015-12-06 14:43:55 -0500

    add files to run run parallel in python

--- To-Compare.py
+++ To-Compare.py
@@ -1,3 +1,9 @@
+# 
+
+import math
+import sys
+import pp
+
 # import twitter API library
 from TwitterAPI import TwitterAPI
 # import regular expression library
@@ -5,158 +11,96 @@ import re
 # import Indico
 import indicoio
 # import GUI
-from Tkinter import *
+# from Tkinter import *
 
-class WhatWeShare:
-	# get authentication
-	api = TwitterAPI('1KxHa1hlPbRdsggvL5yfBgHPY', 'afQVw38uLX3Q1YdILflyG4FjWhjkMzXgSP9ypLee4LM4QIMOea', '2786140432-npYkBKUXdUj3ulYj5f2N7LLN7dVJD6L6KdoyyLi', 'qwOdzFwyNfBKcmO6tq2TbOElrDHfd0ooiXNhMy4a7kUMd')
-	indicoio.config.api_key = 'e2637d8d80afb64412b3dda3dda64bdd'
+def crawlTwits(term):
+    # get authentication
+    api = TwitterAPI('1KxHa1hlPbRdsggvL5yfBgHPY', 'afQVw38uLX3Q1YdILflyG4FjWhjkMzXgSP9ypLee4LM4QIMOea', '2786140432-npYkBKUXdUj3ulYj5f2N7LLN7dVJD6L6KdoyyLi', 'qwOdzFwyNfBKcmO6tq2TbOElrDHfd0ooiXNhMy4a7kUMd')
+    indicoio.config.api_key = 'e2637d8d80afb64412b3dda3dda64bdd'
 
-	# search twits
-	r = api.request('search/tweets', {'q':'Vietnam'})
-	for item in r:
-		# filter out patterns
-		patterns = re.compile(', u\'text\': u\'(.*?), u\'is_quote_status\':')
-		if patterns is None:
-			patterns = re.compile(', u\'text\': u\"(.*?), u\'is_quote_status\':')
-		# search for patterns from twits
-		text = patterns.search(str(item))
-		# if found
-		if text:
-			# group into a text
-			twit = text.group(1)
-			print(twit)
-			# send twit to indico to get sentiment analyzed
-			sentimentNum = indicoio.sentiment_hq(twit)
-			print(sentimentNum)
-			if sentimentNum < 0.3:
-				print("Positive")
-			elif sentimentNum > 0.7:
-				print("Negative")
-			else:
-				print("Neutral")
-			print('\n')
+    # search twits
+    r = api.request('search/tweets', {'q':term})
+    for item in r:
+        # filter out patterns
+        patterns = re.compile(', u\'text\': u\'(.*?)\', u\'is_quote_status\':')
+        if patterns is None:
+            patterns = re.compile(', u\'text\': u\"(.*?), u\'is_quote_status\':')
+        # search for patterns from twits
+        text = patterns.search(str(item))
+        # if found
+        if text:
+            # group into a text
+            twit = text.group(1)
+            print(twit)
+            # send twit to indico to get sentiment analyzed
+            sentimentNum = indicoio.sentiment_hq(twit)
+            print(sentimentNum)
+            if sentimentNum < 0.3:
+                print("Negative")
+            elif sentimentNum > 0.7:
+                print("Positive")
+            else:
+                print("Neutral")
+            print('\n')
 
-def main():
-	# create the window
-	root = Tk()
-	root.title("WhatWeShare")
-	root.geometry("800x500")
-	root.centerWindow()
-	# event loop
-	root.mainloop()
-
-if __name__ == '__main__':
-    main()
+def sum_primes(n):
+    """Calculates sum of all primes below given integer n"""
+    return sum([x for x in xrange(2, n) if isprime(x)])
 
 
+print """Usage: python sum_primes.py [ncpus]
+    [ncpus] - the number of workers to run in parallel,
+    if omitted it will be set to the number of processors in the system"""
+
+# Set up job server
+# tuple of all parallel python servers to connect with
+#  like an array
+ppservers = ()
+#ppservers = ("127.0.0.1:60000", )
+
+if len(sys.argv) > 1:
+    terms = sys.argv[1]
+    inputs = terms.split(',')
+    # Creates jobserver with ncpus workers
+    # build a job server, like a box with worker
+    # make # of workers
+    job_server = pp.Server(len(inputs), ppservers=ppservers)
+else:
+    # Creates jobserver with automatically detected number of workers
+    job_server = pp.Server(ppservers=ppservers)
+
+print "Starting pp with", job_server.get_ncpus(), "workers"
+
+# Submit a job of calulating sum_primes(100) for execution.
+# sum_primes - the function
+# (100,) - tuple with arguments for sum_primes
+# (isprime,) - tuple with functions on which function sum_primes depends
+# ("math",) - tuple with module names which must be imported before
+#             sum_primes execution
+# Execution starts as soon as one of the workers will become available
+# give the boss the job and broadcast job for all
+# job1 = job_server.submit(sum_primes, (100, ), (isprime, ), ("math", ))
+
+# Retrieves the result calculated by job1
+# The value of job1() is the same as sum_primes(100)
+# If the job has not been finished yet, execution will
+# wait here until result is available
+# result = job1()
+
+# print "Sum of primes below 100 is", result
 
 
+# The following submits 8 jobs and then retrieves the results
+# List/vector
+# inputs = (100000, 100100, 100200, 100300, 100400, 100500, 100600, 100700)
+# jobs = [(input, job_server.submit(sum_primes, (input, ), (isprime, ),
+        # ("math", ))) for input in inputs]
+jobs = [(input, job_server.submit(crawlTwits, (input, ), 
+    ("TwitterAPI", "re", "indicoio", ))) for input in inputs]
 
-	# print('Here\'s the item')
-	# print(item)
-	# print('\n')
+for input, job in jobs:
+    print "Twits regarding", input, "are", job()
 
-# take text between {u'contributors': None, u'truncated': False, u'text': u'
-# and , u'is_quote_status': False
+job_server.print_stats()
 
-# import tweepy
-# from tweepy import OAuthHandler
- 
-# consumer_key = '1KxHa1hlPbRdsggvL5yfBgHPY'
-# consumer_secret = 'afQVw38uLX3Q1YdILflyG4FjWhjkMzXgSP9ypLee4LM4QIMOea'
-# access_token = '2786140432-npYkBKUXdUj3ulYj5f2N7LLN7dVJD6L6KdoyyLi'
-# access_secret = 'qwOdzFwyNfBKcmO6tq2TbOElrDHfd0ooiXNhMy4a7kUMd'
- 
-# auth = OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_secret)
- 
-# api = tweepy.API(auth)
-
-
-
-# for status in tweepy.Cursor(api.home_timeline).items(10):
-#     # Process a single status
-#     print(status.text) 
-
-
-# import requests
-# import re
-# r = requests.get('https://twitter.com/mtholyoke/followers')
-# # test = re.findall(b'<a\b[^>]*>(.*?)</a>', r.content)
-# print(r.content)
-
-
-# for status in tweepy.Cursor(api.home_timeline).items(10):
-#     # Process a single status
-#     print(status.text)
-
-
-
-# import requests
-# # import lxml.html
-# import re
-# import urllib
-
-# import ConfigParser
-# from twython import Twython
- 
-# config = ConfigParser.ConfigParser()
-# config.read('scraper.cfg')
- 
-# # spin up twitter api
-# APP_KEY    = config.get('credentials','1KxHa1hlPbRdsggvL5yfBgHPY')
-# APP_SECRET = config.get('credentials','afQVw38uLX3Q1YdILflyG4FjWhjkMzXgSP9ypLee4LM4QIMOea')
-# OAUTH_TOKEN        = config.get('credentials','2786140432-npYkBKUXdUj3ulYj5f2N7LLN7dVJD6L6KdoyyLi')
-# OAUTH_TOKEN_SECRET = config.get('credentials','qwOdzFwyNfBKcmO6tq2TbOElrDHfd0ooiXNhMy4a7kUMd')
- 
-# twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-# twitter.verify_credentials()
-
-
-# username = "sociu2001@gmail.com"
-# password = "20011995"
-
-# class LinkedInParser(object):
-
-#     def __init__(self, login, password):
-#         """ Start up... """
-#         self.login = login
-#         self.password = password
-
-#         # Simulate browser with cookies enabled
-#         # self.cj = cookielib.MozillaCookieJar(cookie_filename)
-#         # if os.access(cookie_filename, os.F_OK):
-#         self.cj.load()
-#         self.opener = urllib.request.build_opener(
-#             urllib.request.HTTPRedirectHandler(),
-#             urllib.request.HTTPHandler(debuglevel=0),
-#             urllib.request.HTTPSHandler(debuglevel=0),
-#             urllib.request.HTTPCookieProcessor(self.cj)
-#         )
-#         self.opener.addheaders = [
-#             ('User-agent', ('Mozilla/4.0 (compatible; MSIE 6.0; '
-#                            'Windows NT 5.2; .NET CLR 1.1.4322)'))
-#         ]
-
-#         # Login
-#         self.loginPage()
-
-#         title = self.loadTitle()
-
-# r = requests.get('https://twitter.com/mtholyoke/followers')
-# test = re.findall(b'<a\b[^>]*>(.*?)</a>', r.content)
-# print(test)
-
-# # tree = lxml.html.fromstring(r.text)
-
-# # import scrapy
-
-# # class MySpider(scrapy.Spider):
-# # 	name = 'https://www.facebook.com/Microsoft'
-# # 	allowed_domains = ['https://www.facebook.com/Microsoft']
-# # 	start_urls = ['https://www.facebook.com/Microsoft',]
-
-# # 	def parse(self, response):
-# # 		msg = 'A response from %s just arrived!' % response.start_urls
-# # 		self.logger.info(msg)
+# Parallel Python Software: http://www.parallelpython.com
