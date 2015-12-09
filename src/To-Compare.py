fb5917311ff6fdfa0c2a3ec891c1e6b5ec132408
Kayla Nguyen <nguye27k@mtholyoke.edu>
2015-12-09 14:24:52 -0500

    add function to get the key words in each twit

--- src/To-Compare.py
+++ src/To-Compare.py
@@ -1,13 +1,12 @@
 # import system module
 import sys
-
 # import parallel module
 import pp
+# import regular expression library
+import re
 
 # import twitter API library
 from TwitterAPI import TwitterAPI
-# import regular expression library
-import re
 # import Indico
 import indicoio
 
@@ -22,6 +21,8 @@ def crawlTwits(term):
     scoreSum = 0
     # keep a counter to sum the number of twits
     twitsNum = 0
+    # keep a list of keywords
+    listKeyWords = ""
 
     # search twits
     r = api.request('search/tweets', {'q':term})
@@ -36,14 +37,26 @@ def crawlTwits(term):
         if text:
             # group into a text
             twit = text.group(1)
-            # print(twit)
+
             # send twit to indico to get sentiment analyzed
             sentimentNum = indicoio.sentiment_hq(twit)
-            # print(sentimentNum)
+            # sent twit to indico to get keywords
+            json_keyWords = indicoio.keywords(twit)
+            # go through dict object
+            for key, value in json_keyWords.items():
+            	# if the key is relevant enough
+            	if value >= 0.2:
+	            	# add keywords to the list
+    	        	listKeyWords += key + ", "
+
             # add up score sum
             scoreSum += sentimentNum
             # increment number of twits
             twitsNum += 1
+
+            # Uncomment lines below to debug
+            # print(twit)
+            # print(sentimentNum)
             # if sentimentNum < 0.3:
             #     print("Negative")
             # elif sentimentNum > 0.7:
@@ -51,48 +64,54 @@ def crawlTwits(term):
             # else:
             #     print("Neutral")
             # print('\n')
+
     # compute the average sentiment score
     average = scoreSum/twitsNum
-    # return the message
-    if average < 0.2:
+    # get the evaluation
+    if average <= 0.2:
         rate = "very negative"
-    elif average <0.4:
+    elif average <= 0.4:
         rate = "slightly negative"
-    elif average > 0.6:
-        rate = "slightly positive"
-    elif average > 0.8:
+    elif average >= 0.8:
         rate = "very positive"
+    elif average >= 0.6:
+    	rate = "slightly positive"
     else:
         rate = "neutral"
     # string to return
-    string = "an average score of " + str(average) + "\nOverall, it is " + str(rate)
+    string = "an average score of " + str(average) + "\nOverall, it is " + str(rate) + "\nKeywords are " + listKeyWords
     return string
 
-# Set up job server
-# tuple of all parallel python servers to connect with
-ppservers = ()
-#ppservers = ("127.0.0.1:60000", )
+def main():
+	# Set up job server
+	# tuple of all parallel python servers to connect with
+	ppservers = ()
+	#ppservers = ("127.0.0.1:60000", )
 
-# take in inputs from user
-terms = raw_input("Give me a list of terms separated with <> to be searched in parallel!\nThe number of terms is the number of processors\n")
-# split them base on commas
-inputs = terms.strip().split("<>")
+	# take in inputs from user
+	terms = raw_input("Give me a list of terms separated with <> to be searched in parallel!\nThe number of terms is the number of processors\n")
+	# split them base on commas
+	inputs = terms.strip().split("<>")
 
-# Creates jobserver with len(inputs) workers
-job_server = pp.Server(len(inputs), ppservers=ppservers)
+	# Creates jobserver with len(inputs) workers
+	job_server = pp.Server(len(inputs), ppservers=ppservers)
 
-print "Starting pp with", job_server.get_ncpus(), "workers"
+	print "Starting pp with", job_server.get_ncpus(), "workers"
 
-# Submit a job of crawling twits for execution.
-# crawTwits - the function
-# (input,) - tuple with arguments for crawTwits
-# () - tuple with functions on which function sum_primes depends
-# ("from TwitterAPI import TwitterAPI", "re", "indicoio", ) 
-# - tuple with module names which must be imported before crawTwits execution
-jobs = [(input, job_server.submit(crawlTwits, (input, ), (),
-    ("from TwitterAPI import TwitterAPI", "re", "indicoio", ))) for input in inputs]
+	# Submit a job of crawling twits for execution.
+	# crawTwits - the function
+	# (input,) - tuple with arguments for crawTwits
+	# () - tuple with functions on which function sum_primes depends
+	# ("from TwitterAPI import TwitterAPI", "re", "indicoio", ) 
+	# - tuple with module names which must be imported before crawTwits execution
+	jobs = [(input, job_server.submit(crawlTwits, (input, ), (),
+	    ("from TwitterAPI import TwitterAPI", "re", "indicoio", ))) for input in inputs]
 
-for input, job in jobs:
-    print "\nTwits regarding", input, "have", job()
+	for input, job in jobs:
+	    print "\nTwits regarding", input, "have", job()
 
-job_server.print_stats()
+	job_server.print_stats()
+
+# run the main method
+if __name__ == '__main__':
+    main()
